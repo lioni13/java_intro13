fbc49d0ff67f1239402120cd83f3901c2134dae8
lioni13 <98702164+lioni13@users.noreply.github.com>
2022-03-05 16:09:32 +0200

    Aded chainges

--- src/main/java/automation/qa/domashno1.java
+++ src/main/java/automation/qa/domashno1.java
@@ -1,2 +1,25 @@
-package automation.qa;public class domashno1 {
-}
+package automation.qa;
+
+import java.util.Scanner;
+
+public class domashno1 {
+
+    public static void main(String[] main) {
+
+        System.out.print("Please enter your first name");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+
+        System.out.print("Enter your last name");
+
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter your favorite hobby");
+
+        String hobby = scanner.nextLine();
+
+        System.out.println("Your first name is" + firstName + " and your last name" + lastName + " and your favorite hobby is " + hobby);
+   }
+ }
